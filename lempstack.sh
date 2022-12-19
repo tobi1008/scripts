88678458e74ba657898e1f338469862b2c6b5b1d
tobi1008 <110457414+tobi1008@users.noreply.github.com>
2022-12-20 01:17:51 +0700

    Create lempstack.sh

---
+++ lempstack.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+yum update -y
+yum install epel-release -y
+yum install nginx -y
+systemctl start nginx
+systemctl enable nginx
+yum install -y mariadb mariadb-server
+systemctl start mariadb
+yum -y install yum-utils
+rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum-config-manager --enable remi-php73
+yum install -y php php-fpm php-ldap php-zip php-embedded php-cli php-mysql php-common php-gd php-xml php-mbstring php-mcrypt php-pdo php-soap php-json php-simplexml php-process php-curl php-bcmath php-snmp php-pspell php-gmp php-intl php-imap perl-LWP-Protocol-https php-pear-Net-SMTP php-enchant php-pear php-devel php-zlib php-xmlrpc php-tidy php-opcache php-cli php-pecl-zip unzip gcc
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php.ini
+sed -i 's|listen = 127.0.0.1:9000|listen = /var/run/php_fpm.sock|g' /etc/php-fpm.d/www.conf
+sed -i 's/;listen.owner = nobody/listen.owner = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/;listen.group = nobody/listen.group = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/user = apache/user = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/group = apache/group = nginx/g' /etc/php-fpm.d/www.conf
+systemctl start php-fpm
+systemctl enable php-fpm
+chmod 666 /var/run/php_fpm.sock
+chown nginx:nginx /var/run/php_fpm.sock
+echo 'server {
+    listen  80;
+    server_name domain_name;
+
+    root   /var/www/html/;
+    index index.php index.html index.htm;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+    error_page 404 /404.html;
+    error_page 500 502 503 504 /50x.html;
+
+    location = /50x.html {
+        root /usr/share/nginx/html;
+    }
+
+    location ~ \.php$ {
+        try_files $uri =404;
+        fastcgi_pass unix:/var/run/php_fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
+' > /etc/nginx/conf.d/default.conf
+systemctl restart nginx
+touch /var/www/html/index.php | echo 'Script by Tobi' >  /var/www/html/index.php
