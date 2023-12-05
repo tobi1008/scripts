4827684f7208da725f3e3d3079a63aac3e1a3718
tobi1008 <110457414+tobi1008@users.noreply.github.com>
2023-12-05 17:15:18 +0700

    Update lempalmalinux.sh

--- lempalmalinux.sh
+++ lempalmalinux.sh
@@ -9,7 +9,7 @@ systemctl start mariadb
 yum -y install yum-utils
 sudo dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm
 sudo dnf module reset php
-sudo dnf module install php:remi-8.0
+sudo dnf module install php:remi-8.1
 yum install -y php php-fpm php-ldap php-zip php-embedded php-cli php-mysql php-common php-gd php-xml php-mbstring php-mcrypt php-pdo php-soap php-json php-simplexml php-process php-curl php-bcmath php-snmp php-pspell php-gmp php-intl php-imap perl-LWP-Protocol-https php-pear-Net-SMTP php-enchant php-pear php-devel php-zlib php-xmlrpc php-tidy php-opcache php-cli php-pecl-zip unzip gcc
 sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php.ini
 sed -i 's|listen = 127.0.0.1:9000|listen = /var/run/php_fpm.sock|g' /etc/php-fpm.d/www.conf
